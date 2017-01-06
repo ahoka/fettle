233d3cfb8ad2a0975eea1df86a4da0b529f6c507
Adam Hoka <adam.hoka@gmail.com>
2017-01-06 15:18:19 +0100

    Validate based on a generated uuid

--- server/fettle.go
+++ server/fettle.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"os"
+
 	"github.com/google/uuid"
 )
 
@@ -12,37 +15,56 @@ type response struct {
 }
 
 type Instance struct {
-	Id UUID
+	ID uuid.UUID
 }
 
-func NewInstance() {
+func NewInstance() Instance {
 	return Instance{uuid.New()}
+}
+
+func writeResponse(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(response{message})
 }
 
 // Start fettle
 func Start() {
+	instance := NewInstance()
+
+	fettleAddress := os.Getenv("FETTLE_ADDRESS")
+	if fettleAddress == "" {
+		fettleAddress = "0.0.0.0"
+	}
+
+	fettlePort := os.Getenv("FETTLE_PORT")
+	if fettlePort == "" {
+		fettlePort = "8099"
+	}
+
+	log.Println("Starting new fettle instance with id", instance.ID)
+
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Req: ", r.URL)
 
 		id, present := r.URL.Query()["id"]
-
-		log.Println(present)
-
 		if present {
 			log.Println("Got id: ", id[0])
 
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(200)
-			json.NewEncoder(w).Encode(response{"OK"})
+			uuid, err := uuid.Parse(id[0])
+			if err != nil || uuid != instance.ID {
+				log.Println("Invalid Id, rejecting")
+				writeResponse(w, "Mismatch", 404)
+			} else {
+				writeResponse(w, "OK", 200)
+			}
 		} else {
 			log.Println("No Id present, rejecting")
-
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(404)
-			json.NewEncoder(w).Encode(response{"Invalid ID"})
+			writeResponse(w, "Mismatch", 404)
 		}
 	})
 
-	log.Println("Listening on 8099")
-	log.Fatal(http.ListenAndServe(":8099", nil))
+	listenAddress := fettleAddress + ":" + fettlePort
+	log.Println("Listening on", listenAddress)
+	log.Fatal(http.ListenAndServe(listenAddress, nil))
 }
